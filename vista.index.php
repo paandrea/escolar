243adc88e6cc62f34cadae33d99081f0e2c100a3
Paula Benavides <pabenavides43@misena.edu.co>
2021-09-21 13:25:23 -0500

    first commit

---
+++ vista.index.php
@@ -0,0 +1,41 @@
+
+<!Doctype html>
+<html lang="es">
+<head>
+	<title>Login | Registros de Notas</title>
+	<link rel="shortcut icon" href="image/escudo.png">
+	
+	
+	
+	
+	
+</head>
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+</html>
+
+
+
+
+
+
+
+
+
